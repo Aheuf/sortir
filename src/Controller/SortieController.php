f63d2897e5352e3bc97665ca619248b8fd096307
emilenasc <nascivetemile@gmail.com>
2021-09-07 13:25:50 +0200

    modification form rechercher

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\rechercheType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +13,15 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie", name="sortie")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $rechercheForm = $this->createForm(rechercheType::class);
+
+        $rechercheForm->handleRequest($request);
+
+
         return $this->render('sortie/index.html.twig', [
+            'rechercheForm' => $rechercheForm->createView()
         ]);
     }
 }
---
+++ src/Form/rechercheType.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace App\Form;
+
+use \Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class rechercheType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('campus', ChoiceType::class, [
+                'choices' => [
+                    'NANTES' => 'Nantes',
+                    'NIORT' => 'Niort',
+                    'RENNES' => 'Rennes',
+                    'QUIMPER' => 'Quimper',
+                ],
+                'label' => 'Campus :',
+                'attr' => [
+                    'class' => 'col-6'
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Le nom contient : ',
+                'attr' => [
+                    'class' => 'col-6'
+                ]
+            ])
+            ->add('debut', DateType::class, [
+                'label' => 'Entre : ',
+                'attr' => [
+                    'class' => 'col-6'
+                ]
+            ])
+            ->add('fin', DateType::class, [
+                'label' => ' et : ',
+                'attr' => [
+                    'class' => 'col-6'
+                ]
+            ])
+            ->add('organisateur', CheckboxType::class, [
+                'label' => 'Sorties dont je suis l\'organisateur',
+                'attr' => [
+                    'class' => 'form-check-input'
+                ]
+            ])
+            ->add('inscrit', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je suis inscrit/e',
+                'attr' => [
+                    'class' => 'form-check-input'
+                ]
+            ])->add('noninscrit', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je ne suis pas inscrit/e',
+                'attr' => [
+                    'class' => 'form-check-input'
+                ]
+            ])->add('passees', CheckboxType::class, [
+                'label' => 'Sorties passées',
+                'attr' => [
+                    'class' => 'form-check-input'
+                ]
+            ]);
+
+    }
+}
