0e7127bb06032835fda8b4301667db6817b0122c
akr75000 <akissrock@gmail.com>
2021-09-07 12:16:15 +0200

    rename et modif sortie-twig en create-twig + modif sortie ctrl

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\CreateSortieType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,12 +24,41 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie/nouvelle_sortie", name="sortie_create")
      */
-    public function create(Request $request): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
+        //notre entité vide
         $sortie = new Sortie();
+
+        //notre formulaire, associée à l'entité vide
         $sortieForm = $this->createForm(CreateSortieType::class, $sortie);
+
+        //récupère les données du form et les injecte dans notre $wish
         $sortieForm->handleRequest($request);
-        return $this->render('sortie/sortie.html.twig', [
+
+        //si le formulaire est soumis et valide...
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            //hydrate les propriétés absentes du formulaires
+            $sortie->setNom(true);
+            $sortie->setDateHeureDebut(new \DateTime());
+            $sortie->setDateLimiteInscription(new \DateTime());
+            $sortie->setNbInscriptionMax(true);
+            $sortie->setDuree(true);
+            $sortie->setInfoSortie(true);
+
+
+            //sauvegarde en bdd
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            //affiche un message sur la prochaine page
+            $this->addFlash('success', 'La sortie a été ajouté avec succès!');
+
+            //redirige vers la page de détails de l'idée fraîchement créée
+            return $this->redirectToRoute('detail_sortie', ['id' => $sortie->getId()]);
+        }
+
+        //affiche le formulaire
+        return $this->render('sortie/create.html.twig', [
             "sortieForm" => $sortieForm->createView()
         ]);
     }
