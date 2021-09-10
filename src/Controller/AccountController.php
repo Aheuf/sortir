3bedfb5864ff843b508f8434b5cb816d4a8083c3
Aheuf <clement.genty@gmail.com>
2021-09-10 10:28:43 +0200

    réparation formulaire updateAccount

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -28,14 +28,18 @@ class AccountController extends AbstractController
     /**
      * @Route ("/update/{id}","")
      */
-    public function update($id, ParticipantRepository $repository,Request $request,CampusRepository $campusRepository, EntityManagerInterface $entityManager){
+    public function update($id, ParticipantRepository $repository,Request $request,CampusRepository $campusRepository,
+                           EntityManagerInterface $entityManager){
         $user = new Participant();
+
         $form =$this->createForm(UpdateAccountType::class,$user);
+        $form->handleRequest($request);
+
         $user = $repository->find($id);
 
         $allCampus = $campusRepository->findAll();
 
-        if ($form->isSubmitted() && $form-> isValid()) {
+        if ($form->isSubmitted() && $form-> isValid() || $form['email']->getData()==$user->getEmail()) {
 
                                                 //gestion rattachement campus
             $campus = $campusRepository->findOneBy(['id'=>$request->get('campus')]);
@@ -43,15 +47,8 @@ class AccountController extends AbstractController
 
             $user->setNom($form['nom']->getData());
             $user->setPrenom($form['prenom']->getData());
-
-            if ($form['pseudo']->getData() != $user->getPseudo()){
-                $user->setPseudo($form['pseudo']->getData());
-            }
-
-            if ($form['email']->getData() != $user->getEmail()){
-                $user->setEmail($form['email']->getData());
-            }
-
+            $user->setPseudo($form['pseudo']->getData());
+            $user->setEmail($form['email']->getData());
             $user->setTelephone($form['telephone']->getData());
 
                                                         //gestion de l'image
@@ -65,13 +62,10 @@ class AccountController extends AbstractController
                 $folder = './img';
                 $avatar->move($folder, $nom);
                 $user->setAvatar($nom);
-            } else {
-                $avatar = $user->getAvatar();
-                $user->setAvatar($avatar);
             }
 
             $entityManager->flush();
-            $this->addFlash('sucess','votre compte à été modifié');
+            $this->addFlash('success','votre compte à été modifié');
             return $this->redirectToRoute('app_account_show',['id' => $id]);
         }
 
